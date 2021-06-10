66dca983148a699cc8fac6210ebccc8d303a7fc7
seo <vpflt992@gmail.com>
2021-06-10 13:05:35 +0900

    Merge remote-tracking branch 'origin/master'

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from kafka import KafkaConsumer
 from json import loads
+import time
  
   # topic, broker list 
-consumer = KafkaConsumer( 'topic1', 
-bootstrap_servers=['localhost:9092'], 
-auto_offset_reset='earliest', 
-enable_auto_commit=True, 
-group_id='my-group', 
-consumer_timeout_ms=1000 ) # consumer list를 가져온다
+consumer = KafkaConsumer('topic1', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group', consumer_timeout_ms=1000 ) # consumer list를 가져온다
 print('[begin] get consumer list') 
 
 while True:
-    for message in consumer:
-        print("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s" % ( message.topic, message.partition, message.offset, message.key, message.value ))
-        print('[end] get consumer list')
+  time.sleep(3)
+  for message in consumer:
+    print("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s" % ( message.topic, message.partition, message.offset, message.key, message.value ))
+    print('[end] get consumer list')
+    s1=str(message.value).split(":")
+    print(s1)
+    if(s1[0]=="reserved"):
+      print("reserved"+s1[1:])
+    elif(s1[0]=="cancelled"):
+      print("cancelled")
+    else:
+      print(s1)
 
 
